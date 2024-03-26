16d236cd8f972dadec6eaf6b4c2d54935a3f314b
Axel <axelcroizepro@gmail.com>
2024-03-26 22:05:37 +0100

    Set 3 meshes with 3 animations, testing to find which are good enough

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
-import testVertexShader from './shaders/test/vertex.glsl'
-import testFragmentShader from './shaders/test/fragment.glsl'
+import vertexShader from './shaders/vertex.glsl'
+import mainFragmentShader from './shaders/fragment.glsl'
+import squareFragmentShader from './shaders/fragment_square.glsl'
+import discFragmentShader from './shaders/fragment_disc.glsl'
 
 /**
  * Base
@@ -21,15 +23,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test mesh
+ * Objects
  */
+
+// First
+
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
-const material = new THREE.ShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
+const material1 = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: mainFragmentShader,
     side: THREE.DoubleSide,
     transparent: true,
     uniforms:
@@ -39,9 +44,45 @@ const material = new THREE.ShaderMaterial({
 })
 
 // Mesh
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const mesh1 = new THREE.Mesh(geometry, material1)
+scene.add(mesh1)
 
+// Second
+const material2 = material1.clone()
+material2.fragmentShader = squareFragmentShader
+const mesh2 = new THREE.Mesh(geometry, material2)
+mesh2.scale.set(0.75,0.75,0.75)
+mesh2.position.set(0, -1, 0)
+scene.add(mesh2)
+
+// Third
+const material3 = material1.clone()
+material3.fragmentShader = discFragmentShader
+const mesh3 = new THREE.Mesh(geometry, material3)
+mesh3.scale.set(0.45,0.45,0.45)
+// mesh3.rotateY(-Math.PI/8)
+mesh3.position.set(-0.05, -2.05, 0)
+scene.add(mesh3)
+
+
+const sectionMeshes = [mesh1,mesh2,mesh3] 
+const animations = {
+    0 : null,
+    1: {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: '+=3',
+        y: '+=3',
+        z: '+=0'
+    },
+    2: {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: '+=6',
+        y: '+=6',
+        z: '+=0'
+    },
+}
 
 /**
  * Sizes
@@ -64,15 +105,12 @@ window.addEventListener('scroll', () =>
     {
         currentSection = newSection
 
+        if(!animations[currentSection])
+            return;
+
         gsap.to(
             sectionMeshes[currentSection].rotation,
-            {
-                duration: 1.5,
-                ease: 'power2.inOut',
-                x: '+=6',
-                y: '+=3',
-                z: '+=1.5'
-            }
+            animations[currentSection]
         )
     }
 })
@@ -128,14 +166,48 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime
 
+function clamp(value, min, max)
+{
+    let clamped = value > min ? value : min;
+    clamped = clamped < max ? clamped : max 
+
+    return clamped;
+}
+
+function lerp (start, end, amt){
+    return (1-amt)*start+amt*end
+  }
+
+let animation_forwards = true
+let animation_value = 0
 const tick = () =>
 {
 
     const elapsedTime = clock.getElapsedTime()
+    if(Math.floor(elapsedTime) != Math.floor(previousTime))
+    {
+        previousTime = elapsedTime
+    }
+
+    if(animation_forwards)
+        // animation_value = lerp(animation_value, 40, 0.01);
+        animation_value += 0.03;
+    else 
+        // animation_value = lerp(animation_value, 0, 0.01);
+        animation_value -= 0.03;
+
+    if(animation_forwards && animation_value >= 40)
+        animation_forwards = false;
+    else if(!animation_forwards && animation_value <= 0)
+        animation_forwards = true
+
+    // Update materials
+    material1.uniforms.uTime.value = animation_value
+
+    // mesh3.rotateY(-Math.PI/8 / 190)
     
-    // Update material
-    material.uniforms.uTime.value = elapsedTime
 
     // Animate camera
     camera.position.y = - scrollY / sizes.height * parameters.objectsDistance
