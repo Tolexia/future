aa171bb62c392b8cc5b876873cfa73b0e8de9220
Axel <axelcroizepro@gmail.com>
2024-04-01 13:40:24 +0200

    new attempt to fix page ump when toolbar hides

--- src/script.js
+++ src/script.js
@@ -271,8 +271,8 @@ window.addEventListener('resize', () =>
     camera.updateProjectionMatrix()
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // renderer.setSize(sizes.width, sizes.height)
+    // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
